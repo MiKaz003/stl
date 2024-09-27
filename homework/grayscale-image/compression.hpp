5147c9c780ec06f82bba6db2902bd3eaa7dfca7b
Michał Kaźmierczak <ghmkazmierczak@gmail.com>
2024-09-27 17:11:13 +0200

    adding formated

--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -1,13 +1,11 @@
 #pragma once
-#include <vector>
-#include <utility>
-#include <cstdint>
-#include <array>
 #include <stddef.h>
-
-
+#include <array>
+#include <cstdint>
+#include <utility>
+#include <vector>
 
 constexpr size_t width = 32;
 constexpr size_t height = 32;
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height>& img);
-std::array<std::array<uint8_t, 32>, 32>  decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>>& input);
+std::array<std::array<uint8_t, 32>, 32> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>>& input);
