b86c269405288623b4f2bb9ed0c67f5c31bb7873
Michał Kaźmierczak <ghmkazmierczak@gmail.com>
2025-04-01 14:56:00 +0200

    Formated

--- homework/insensitive-palindrom/detector.cpp
+++ homework/insensitive-palindrom/detector.cpp
@@ -1,29 +1,30 @@
 #include "detector.hpp"
 #include <algorithm>
 
-void toLowerCase(std::string& word){
-    std::transform(word.begin(), word.end(), word.begin(), [](char c){
+void toLowerCase(std::string& word) {
+    std::transform(word.begin(), word.end(), word.begin(), [](char c) {
         return std::tolower(c);
     });
 }
 
-void removeSpecialCases(std::string& word){
-    word.erase(std::remove_if(word.begin(), word.end(), [](char c){
-        return !std::isalnum(c);
-    }), word.end());
+void removeSpecialCases(std::string& word) {
+    word.erase(std::remove_if(word.begin(), word.end(), [](char c) {
+                   return !std::isalnum(c);
+               }),
+               word.end());
 }
 
-bool is_palindrome(std::string word){
+bool is_palindrome(std::string word) {
     toLowerCase(word);
     removeSpecialCases(word);
     auto begin = word.begin();
     auto end = word.rbegin();
 
-    while(begin < word.end() - (word.size() / 2)){ 
-        if(*begin == *end){ 
+    while (begin < word.end() - (word.size() / 2)) {
+        if (*begin == *end) {
             begin = std::next(begin);
             end = std::next(end);
-        } else{
+        } else {
             return false;
         }
     }
