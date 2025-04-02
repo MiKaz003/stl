1391535471baf0f096f8801e832f383a9559c300
Michał Kaźmierczak <ghmkazmierczak@gmail.com>
2025-04-02 23:53:58 +0200

    Erase and shrink_to_fit added

--- homework/transform-containers/transform-containers.cpp
+++ homework/transform-containers/transform-containers.cpp
@@ -15,8 +15,9 @@ std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string
     std::map<int, std::string> result;
     list.sort();
     std::sort(deque.begin(), deque.end());
-    std::unique(list.begin(), list.end());
-    std::unique(deque.begin(), deque.end());
+    list.erase(std::unique(list.begin(), list.end()), list.end());
+    deque.erase(std::unique(deque.begin(), deque.end()), deque.end());
+    deque.shrink_to_fit();
 
     std::transform(deque.begin(), deque.end(), list.begin(), std::inserter(result, result.begin()), [](auto& lhs, auto& rhs) {
         return std::make_pair(lhs, rhs);
